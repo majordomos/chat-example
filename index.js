75cc749682bc906c160ebc9c9c603b2fde339737
Alexey Mitsenko <alex.micenko@gmail.com>
2021-11-05 13:17:54 +0300

    test comm

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connect', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+
+});
+
+http.listen(port, () => {
+    console.log('listening to 3000');
+});
